57c7a1e6b4a59bd9372242bc8a4b861882f9923b
MrGago <cleamen@free.fr>
2019-11-22 00:01:00 +0100

    Added Cursor v.1 (NEEDS A FIX!)

---
+++ wW_scrapeTheLabel/Assets/Scripts/CursorDisplayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorDisplayer : MonoBehaviour
+{
+    //Public
+    public SpriteRenderer sprRenderer;
+
+    public Sprite sprIdle;
+    public Sprite sprScraping;
+    public Sprite sprLose;
+    public Sprite sprWin;
+
+
+    //Private
+
+
+    private void Awake()
+    {
+        sprRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnGUI()
+    {
+        if(GameManager.Instance.playerIsScraping || GameManager.Instance.playerTrapped)
+        {
+            sprRenderer.sprite = sprScraping;
+        }
+        else
+        {
+            sprRenderer.sprite = sprIdle;
+        }
+    }
+}
